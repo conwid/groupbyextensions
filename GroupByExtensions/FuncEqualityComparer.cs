a983d3f00e792d0f8cd0767de90064da68392bf2
conwid <nagy.akos.bme.2009@gmail.com>
2017-11-15 23:53:00 +0100

    Add files via upload

---
+++ GroupByExtensions/FuncEqualityComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupByExtensions
+{
+    public class FuncEqualityComparer<T, TValue> : IEqualityComparer<T>
+    {
+        private readonly Func<T, TValue> extractorFunc;
+        public FuncEqualityComparer(Func<T, TValue> extractorFunc)
+        {
+            this.extractorFunc = extractorFunc;
+        }
+        public bool Equals(T x, T y)
+        {
+            return extractorFunc(x).Equals(extractorFunc(y));
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return extractorFunc(obj).GetHashCode();
+        }
+    }
+}
---
+++ GroupByExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupByExtensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<IGrouping<TKey, TSource>> GroupBy<TSource, TKey, TExtractor>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TKey, TExtractor> extractorFunc)
+        {
+            return source.GroupBy(keySelector, new FuncEqualityComparer<TKey, TExtractor>(extractorFunc));
+        }
+        public static IEnumerable<IGrouping<TKey, TElement>> GroupBy<TSource, TKey, TElement, TExtractor>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, Func<TKey, TExtractor> extractorFunc)
+        {
+            return source.GroupBy(keySelector, elementSelector, new FuncEqualityComparer<TKey, TExtractor>(extractorFunc));
+        }
+        public static IEnumerable<TResult> GroupBy<TSource, TKey, TResult, TExtractor>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TKey, IEnumerable<TSource>, TResult> resultSelector, Func<TKey, TExtractor> extractorFunc)
+        {
+            return source.GroupBy(keySelector, resultSelector, new FuncEqualityComparer<TKey, TExtractor>(extractorFunc));
+        }
+
+        public static IEnumerable<TResult> GroupBy<TSource, TKey, TElement, TResult, TExtractor>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, Func<TKey, IEnumerable<TElement>, TResult> resultSelector, Func<TKey, TExtractor> extractorFunc)
+        {
+            return source.GroupBy(keySelector, elementSelector, resultSelector, new FuncEqualityComparer<TKey, TExtractor>(extractorFunc));
+        }
+
+    }
+}
